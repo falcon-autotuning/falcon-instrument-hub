560a81b4c06f47a25f153afb6098c899c43a0de4
tjk144 <tkovach@wisc.edu>
2025-05-21 13:58:24 -0500

    Added a Buffered Current Sink instrument that supports buffered measurements.

---
+++ src/instrument_server/instrument_drivers/buffered_current_sink.py
@@ -0,0 +1,17 @@
+"""A buffered current sink instrument driver."""
+
+from .dc_current_sink import DCCurrentSink
+from .constants import SUPPORTED_PROPERTIES
+
+
+class BufferedCurrentSink(DCCurrentSink):
+    """A buffered current sink driver."""
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Initialize the buffered current sink driver."""
+        super().__init__(*args, **kwargs)
+        self.program_property(
+            property_name=SUPPORTED_PROPERTIES.SUPPORTS_BUFFERED_MEASUREMENTS,
+            index=self._global_index,
+            get_cmd=lambda: True,
+        )
