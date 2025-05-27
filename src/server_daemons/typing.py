dab0f205a45edfe3bd1fc9f9bad25d0793bac0bd
tjk144 <tkovach@wisc.edu>
2025-05-26 22:46:31 -0500

    Finished isolating server_daemons to properly run with a submodule, instrument_templates

--- src/server_daemons/typing.py
+++ src/server_daemons/typing.py
@@ -1,42 +1,41 @@
 """Typing for the messaging daemon module."""
 
 from collections.abc import Awaitable, Callable
-from typing import TYPE_CHECKING, Any
+from typing import Any
 
-from falcon_core.instrument_interfaces.instrument import Instrument
 from falcon_core.instrument_interfaces.names import InstrumentPort
+from falcon_core.math.arrays.base_array import BaseArray
+from falcon_core.math.arrays.measured_array_1D import MeasuredArray1D
 from falcon_core.physics.device_structures import Connection
+from falcon_core.typing import Instrument
+from instrument_templates.base_instrument_driver import BaseInstrumentDriver
+from instrument_templates.typing import Index, PropertyJson, PropertyName, PropertyValue
 from nats.aio.client import Client
 from nats.aio.msg import Msg
 from numpy.typing import NDArray
 
-from ..instrument_drivers.base_instrument_driver import BaseInstrumentDriver
-from ..instrument_drivers.typing import Index, PropertyJson, PropertyName, PropertyValue
-
-if TYPE_CHECKING:
-    from typing import TypeAlias
-
-Dimension: "TypeAlias" = int
-DimensionIndex: "TypeAlias" = str
-ConnectionName: "TypeAlias" = str
-Data: "TypeAlias" = str
-AxisLabel: "TypeAlias" = str
-Unit: "TypeAlias" = str
-AxisMetadata: "TypeAlias" = dict[str, Unit | int]
-AxisLabels: "TypeAlias" = dict[AxisLabel, AxisMetadata]
-Domain: "TypeAlias" = dict[str, Data | AxisLabels]
-Range: "TypeAlias" = dict[str, Data | Unit]
-Ranges: "TypeAlias" = dict[AxisLabel, Range]
-Domains: "TypeAlias" = dict[DimensionIndex, Domain]
-Dimensions: "TypeAlias" = dict[DimensionIndex, Dimension]
-Metadata: "TypeAlias" = dict[str, str | int | float]
-ID: "TypeAlias" = str
+type Dimension = int
+type DimensionIndex = str
+type ConnectionName = str
+type Data = str
+type AxisLabel = str
+type Unit = str
+type AxisMetadata = dict[str, Unit | int]
+type AxisLabels = dict[AxisLabel, AxisMetadata]
+type Domain = dict[str, Data | AxisLabels]
+type Range = dict[str, Data | Unit]
+type Ranges = dict[AxisLabel, Range]
+type Domains = dict[DimensionIndex, Domain]
+type Dimensions = dict[DimensionIndex, Dimension]
+type Metadata = dict[str, str | int | float]
+type ID = str
 __all__ = [
     "Connection",
+    "BaseArray",
+    "MeasuredArray1D",
     "NDArray",
     "InstrumentPort",
     "Instrument",
-    "ID",
     "PropertyName",
     "Index",
     "Client",
