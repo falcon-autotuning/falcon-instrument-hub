8c45f55a503fa89030525b180d5e90b924666b78
tjk144 <tkovach@wisc.edu>
2025-06-03 12:42:49 -0500

    Fixed the go.mod file to support the instrument=-server

--- api-update.sh
+++ api-update.sh
@@ -7,7 +7,6 @@ go install github.com/falcon-autotuning/falcon-api/cmd/py-api-loader@dev
 go install github.com/falcon-autotuning/falcon-api/cmd/go-api-loader@dev
 
 py-api-loader --repo=server-interpreter --output=./src/server_daemons/api/interpreter.py
-go-api-loader --repo=server-interpreter --output=./internal/api/interpreter.py
-
 py-api-loader --repo=instrument-templates --output=./src/server_daemons/api/instrument.py
-go-api-loader --repo=instrument-templates --output=./internal/api/instrument.py
+
+go-api-loader --repo=instrument-server --output=./runtime/internal/api/api.go
--- runtime/go.mod
+++ runtime/go.mod
@@ -1,4 +1,4 @@
-module instrument-server
+module github.com/falcon-autotuning/instrument-server/runtime
 
 go 1.24.2
 
