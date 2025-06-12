4efc2176e08fb0c2ee5382ec4a768543f0de2195
tjk144 <tkovach@wisc.edu>
2025-06-12 00:48:55 -0500

    Added deployment log

--- tests/integration/test_full_system.py
+++ tests/integration/test_full_system.py
@@ -667,7 +667,7 @@ async def test_full_measurement_flow(
     cleanup_instruments,
 ):
     """Test a complete measurement flow from request to data upload."""
-    max_wait_time = 2.2
+    max_wait_time = 8.2
     check_interval = 1.0
     elapsed_time = 0.0
     upload_msgs = []
