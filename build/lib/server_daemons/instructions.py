0f63cc75acd37665b2c03e62ee12dd8062d2b0cc
tjk144 <tkovach@wisc.edu>
2025-06-16 23:38:33 -0500

    Updated handling of most commands except for the RETURN_DATA since that depends on if an instrument is a setter or a getter

---
+++ build/lib/server_daemons/instructions.py
@@ -0,0 +1,127 @@
+"""The instuctions for a measurement interpretet."""
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .typing import (
+        Getters,
+        InstrumentPort,
+        PropertyName,
+        PropertyValue,
+        Setters,
+    )
+
+
+class Instruction:
+    """A class that holds a single instruction for a measurement.
+
+    A measurement is a thing that needs to be executed on instrument daemons running on the runtime instrument server.
+    It is broken down into a atomic instructions that can be executed on order on the instruments to satify the measurement.
+    This consists of two main parts:
+        setters: A dictionary of connections and their properties to set.
+        getters: A list of connections to get data from.
+    Getters are no required, but setters are, otherwise there is no measurement to perform.
+    """
+
+    _setters: "Setters"
+    _getters: "Getters"
+    _buffered: bool
+
+    def __init__(
+        self,
+        setters: "Setters" = {},
+        getters: "Getters" = [],
+        buffered: bool = False,
+    ):
+        """Initialize the instruction."""
+        self._getters = getters
+        self._setters = setters
+        self._buffered = buffered
+
+    @property
+    def getters(self) -> "Getters":
+        """The getters for the instruction."""
+        return self._getters
+
+    def add_getter(
+        self,
+        instrument: "InstrumentPort",
+    ) -> None:
+        """Add a getter to the instruction."""
+        self._getters.append(instrument)
+
+    @property
+    def setters(self) -> "Setters":
+        """The setters for the instruction."""
+        return self._setters
+
+    def add_setter(
+        self,
+        instrument: "InstrumentPort",
+        properties: dict["PropertyName", "PropertyValue"],
+    ) -> None:
+        """Add a setter to the instruction."""
+        self._setters[instrument] = properties
+
+    @property
+    def buffered(self) -> bool:
+        """If the instruction is buffered."""
+        return self._buffered
+
+    def __repr__(self) -> str:
+        """Returns a string representation of the instruction."""
+        return (
+            f"Instruction(setters={self._setters}, getters={self._getters}, "
+            f"buffered={self._buffered})"
+        )
+
+
+class MeasurementInstructions:
+    """A class that holds the instructions for a measurement.
+
+    Typically a measurement consists of a list of steps, each step containing directions on what instruments to use, what to set and what to collect, if anything.
+    This class is used to store all of the steps in the form of a measurement.
+    """
+
+    _instructions: list[Instruction]
+
+    def __init__(self, instructions: list[Instruction] = []):
+        """Starts up the insructions object."""
+        self._instructions = instructions
+
+    @property
+    def instructions(self) -> list[Instruction]:
+        """Returns the instructions for the measurement."""
+        return self._instructions
+
+    def __iter__(self):
+        """Iterate over the instructions."""
+        return iter(self._instructions)
+
+    def __len__(self):
+        """Returns the length of the instructions."""
+        return len(self._instructions)
+
+    def __getitem__(self, index: int) -> Instruction:
+        """Gets a specific instruction."""
+        return self._instructions[index]
+
+    def __setitem__(self, index: int, value: Instruction) -> None:
+        """Sets a specific instruction."""
+        self._instructions[index] = value
+
+    def __delitem__(self, index: int) -> None:
+        """Deletes a specific instruction."""
+        self._instructions.pop(index)
+
+    def __contains__(self, item: Instruction) -> bool:
+        """Checks if an instruction is in the list."""
+        return item in self._instructions
+
+    def __repr__(self) -> str:
+        """Returns a string representation of the instructions."""
+        return f"MeasurementInstructions({self._instructions})"
+
+    def __str__(self) -> str:
+        """Returns a string representation of the instructions."""
+        return f"MeasurementInstructions({self._instructions})"
---
+++ build/lib/server_daemons/typing.py
@@ -0,0 +1,58 @@
+"""Typing for the messaging daemon module."""
+
+from collections.abc import Awaitable, Callable
+from typing import Any
+
+from falcon_core.instrument_interfaces.names import InstrumentPort
+from falcon_core.math.arrays.base_array import BaseArray
+from falcon_core.math.arrays.measured_array_1D import MeasuredArray1D
+from falcon_core.physics.device_structures import Connection
+from falcon_core.typing import Instrument
+from instrument_templates.base_instrument_driver import BaseInstrumentDriver
+from instrument_templates.typing import (
+    Index,
+    PropertyJson,
+    PropertyName,
+    PropertyValue,
+    Staircase,
+)
+from nats.aio.client import Client
+from nats.aio.msg import Msg
+from numpy.typing import NDArray
+
+type Dimension = int
+type DimensionIndex = str
+type ConnectionName = str
+type Data = str
+type AxisLabel = str
+type Unit = str
+type AxisMetadata = dict[str, Unit | int]
+type AxisLabels = dict[AxisLabel, AxisMetadata]
+type Domain = dict[str, Data | AxisLabels]
+type Range = dict[str, Data | Unit]
+type Ranges = dict[AxisLabel, Range]
+type Domains = dict[DimensionIndex, Domain]
+type Dimensions = dict[DimensionIndex, Dimension]
+type Metadata = dict[str, str | int | float]
+type ID = int
+type Getters = list[InstrumentPort]
+type Setters = dict[InstrumentPort, dict[PropertyName, PropertyValue]]
+__all__ = [
+    "Connection",
+    "BaseArray",
+    "MeasuredArray1D",
+    "NDArray",
+    "InstrumentPort",
+    "Instrument",
+    "PropertyName",
+    "Index",
+    "Client",
+    "Any",
+    "Msg",
+    "Awaitable",
+    "Callable",
+    "PropertyJson",
+    "PropertyValue",
+    "BaseInstrumentDriver",
+    "Staircase",
+]
