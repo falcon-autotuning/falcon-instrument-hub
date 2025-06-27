00908bd813413b35807f7112c5fe84904f2a14a4
tjk144 <tkovach@wisc.edu>
2025-06-27 16:14:31 -0500

    Updated the linear test

--- tests/integration/test_linear_data.py
+++ tests/integration/test_linear_data.py
@@ -8,8 +8,7 @@ import numpy as np
 import pytest
 from falcon_core.communications.messages import MeasurementRequest
 from falcon_core.constants import INSTRUMENT_TYPES
-from falcon_core.instrument_interfaces.names import Knob, Meters
-from falcon_core.instrument_interfaces.names.ports import Ports
+from falcon_core.instrument_interfaces.names import Knob, Knobs, Meters
 from falcon_core.instrument_interfaces.port_transforms.constant_transform import (
     ConstantTransform,
 )
@@ -283,7 +282,7 @@ def measurement_request(
             units=Units.SECOND,
         )
     )
-    transform = ConstantTransform(ports=Ports(knobs + [meters[0]]), scale=1.0)
+    transform = ConstantTransform(ports=Knobs(knobs), scale=1.0)
     return MeasurementRequest(
         message="test measurement",
         measurement_name="integration_test",
